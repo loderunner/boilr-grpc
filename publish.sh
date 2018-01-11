622939a2cbc0e93c752f208f1d22f00f9cf65d06
Charles Francoise <charles.francoise@gmail.com>
2018-01-11 21:38:42 +0100

    create README.md with publish script

--- publish.sh
+++ publish.sh
@@ -4,9 +4,16 @@ boilr template save . grpc -f           # Save local template to boilr repositor
 cd "$GOPATH"/src/github.com/loderunner 
 rm -rf echo
 boilr template use grpc echo -f         # Generate repo from template
+cd echo
+
+# Write README.md
+cat > README.md <<EOF
+# \`echo\`
+
+Template for a gRPC/REST Go project. Generated with [boilr-grpc](https://github.com/loderunner/boilr-grpc).
+EOF
 
 # Commit and push
-cd echo
 git init
 git remote add origin git@github.com:loderunner/echo.git
 git add .
