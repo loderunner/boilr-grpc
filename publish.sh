251662eb8ef63be3eb121fe5b945fa8157a3ff1c
Charles Francoise <charles.francoise@gmail.com>
2018-01-11 14:30:45 +0100

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,12 @@
+boilr template save . grpc -f           # Save local template to boilr repository
+cd "$GOPATH"/src/github.com/loderunner 
+rm -rf echo
+boilr template use grpc echo -f         # Generate repo from template
+
+# Commit and push
+cd echo
+git init
+git remote add origin git@github.com:loderunner/echo.git
+git add .
+git commit -m 'First import'
+git push -f -u origin master
